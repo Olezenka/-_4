dbf72dcee78bdd8827c026a04a3fe58f4d2a0c1f
Admin <Admin@DESKTOP-MAHVHFA>
2025-12-07 17:24:21 +0300

    ебеъебе еъ ъбебе

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,211 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.pavilionDataGridView = new System.Windows.Forms.DataGridView();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pavilionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pavilionDataGridView
+            // 
+            this.pavilionDataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pavilionDataGridView.AutoGenerateColumns = false;
+            this.pavilionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pavilionDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7});
+            this.pavilionDataGridView.DataSource = this.pavilionBindingSource;
+            this.pavilionDataGridView.Location = new System.Drawing.Point(12, 299);
+            this.pavilionDataGridView.Name = "pavilionDataGridView";
+            this.pavilionDataGridView.RowHeadersWidth = 82;
+            this.pavilionDataGridView.RowTemplate.Height = 33;
+            this.pavilionDataGridView.Size = new System.Drawing.Size(1115, 426);
+            this.pavilionDataGridView.TabIndex = 1;
+            this.pavilionDataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.pavilionDataGridView_CellContentClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Times New Roman", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBox1.Location = new System.Drawing.Point(325, 204);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(215, 37);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.Text = "По убыванию";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 208);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(200, 33);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.No;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(188, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 42);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Фильтрация";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.No;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(746, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(201, 42);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сортировка";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.No;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(664, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Статус";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(870, 208);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 31);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Num_pav";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Num_pav";
+            this.dataGridViewTextBoxColumn1.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ID_mall";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID_mall";
+            this.dataGridViewTextBoxColumn2.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Floor";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Floor";
+            this.dataGridViewTextBoxColumn3.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Status";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn4.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            this.dataGridViewTextBoxColumn4.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Square";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Square";
+            this.dataGridViewTextBoxColumn5.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            this.dataGridViewTextBoxColumn5.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Cost_meter";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Cost_meter";
+            this.dataGridViewTextBoxColumn6.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            this.dataGridViewTextBoxColumn6.Width = 200;
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "Coeff_cost";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Coeff_cost";
+            this.dataGridViewTextBoxColumn7.MinimumWidth = 10;
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            this.dataGridViewTextBoxColumn7.Width = 200;
+            // 
+            // pavilionBindingSource
+            // 
+            this.pavilionBindingSource.DataSource = typeof(РКИСЛР_4.FolderModel.Pavilion);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1139, 737);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pavilionDataGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.BindingSource pavilionBindingSource;
+        private System.Windows.Forms.DataGridView pavilionDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using РКИСЛР_4.FolderModel;
 
 namespace РКИСЛР_4
 {
@@ -15,6 +17,63 @@ namespace РКИСЛР_4
         public Form1()
         {
             InitializeComponent();
+        }
+
+        ModelEF model = new ModelEF();
+        List<Pavilion> pavilions = new List<Pavilion>();
+        List<Pavilion> pavilionChanges = new List<Pavilion>();
+        List<string> pavilionProp = new List<string>();
+
+        private void loadStartData()
+        {
+            pavilionBindingSource.DataSource = pavilionChanges;
+        }
+
+        private void LoadDataCombo()
+        {
+            pavilionProp = typeof(Pavilion).GetProperties().Select(p => p.Name).ToList();
+            pavilionProp.RemoveRange(pavilionProp.Count - 2, 2);
+            comboBox1.DataSource = pavilionProp;
+            comboBox1.SelectedIndex = 0;
+        }
+        private void LoadOrder()
+        {
+            pavilionChanges = checkBox1.Checked ?
+            pavilionChanges.OrderByDescending(p => p.GetType().GetProperties().First(x => x.Name == comboBox1.SelectedItem.ToString()).GetValue(p)).ToList() : pavilionChanges.OrderBy(p => p.GetType().GetProperties().First(x => x.Name == comboBox1.SelectedItem.ToString()).GetValue(p)).ToList();
+            loadStartData();
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            pavilionChanges = pavilions.Where(x => x.Status.Contains(textBox1.Text)).ToList();
+            LoadOrder();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pavilionChanges = pavilions = model.Pavilion.ToList();
+            loadStartData();
+            LoadDataCombo();
+        }
+
+        private void pavilionDataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
